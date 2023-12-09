d4b281c8bb4b6503fe706c592d8a6c12c42629fa
rachitest <rachit-sabharwal@outlook.com>
2023-12-09 01:48:09 -0600

    add documentation to coordinate_descent_algo_alt.cpp to prepare for package creation

--- coordinate_descent_algo_alt.cpp
+++ coordinate_descent_algo_alt.cpp
@@ -3,13 +3,43 @@
 
 using namespace Rcpp;
 
-// Soft thresholding function
+//' Soft thresholding function
+  //'
+  //' @param x A numeric value.
+  //' @param a A numeric value for threshold.
+  //' @return The soft-thresholded value.
+  //'
+  //' @examples
+  //' soft_thresholding(1.5, 1)
+  //' soft_thresholding(-1.5, 1)
+  //' soft_thresholding(0.5, 1)
 double soft_thresholding(double x, double a){
   if (x > a) return (x - a);
   if (x < -a) return (x + a);
   return 0.0;
 }
 
+//' Coordinate Gradient Descent
+  //'
+  //' @param X A matrix of predictors of dimension n x p.
+  //' @param b A vector of responses of length p.
+  //' @param lambda A numeric value for regularization.
+  //' @param tol A numeric value for tolerance.
+  //' @param maxiter An integer for maximum number of iterations.
+  //' @return A vector of coefficients of length p.
+  //'
+  //' @examples
+  //' \donotrun {
+  //' set.seed(2023)
+  //' n <- 100
+  //' p <- 5
+  //' b <- rep(0, p)
+  //' b[1] <- 1
+  //' X <- matrix(rnorm(n * p), nrow = n, ncol = p) 
+  //' lambda <- 0.2
+  //' cg_cd_result = cg_cd(X, b, lambda)
+  //' print(cg_cd_result)
+  //' }
 // [[Rcpp::export]]
 arma::vec cg_cd(arma::mat X, arma::vec b, double lambda, double tol=1e-4, int maxiter=1000){
   int p = X.n_cols;
@@ -33,6 +63,28 @@ arma::vec cg_cd(arma::mat X, arma::vec b, double lambda, double tol=1e-4, int ma
   return u;
 }
 
+//' Coordinate Gradient Descent with Elastic Net Regularization
+ //'
+ //' @param X A matrix of predictors of dimension n x p.
+ //' @param b A vector of responses of length p.
+ //' @param lambda A numeric value for regularization.
+ //' @param alpha A numeric value for elastic net mixing parameter.
+ //' @param tol A numeric value for tolerance.
+ //' @param maxiter An integer for maximum number of iterations.
+ //' @return A vector of coefficients of length p.
+ //'
+ //' @examples
+ //' \donotrun {
+ //' set.seed(2023)
+ //' n <- 100
+ //' p <- 5
+ //' b <- rep(0, p)
+ //' b[1] <- 1
+ //' X <- matrix(rnorm(n * p), nrow = n, ncol = p) 
+ //' lambda <- 0.2
+ //' cg_cd_result = cg_cd_alpha(X, b, lambda)
+ //' print(cg_cd_result)
+ //' }
 // [[Rcpp::export]]
 arma::vec cg_cd_alpha(arma::mat X, arma::vec b, double lambda, double alpha=0, double tol=1e-4, int maxiter=1000){
   int p = X.n_cols;
